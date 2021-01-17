4eeb02f7acbdb2a04e42c8178daba1969e3d3f1e
Jacob Lee <JLpython@outlook.com>
2021-01-17 12:51:29 -0800

    Add reset button to reset window to original state

--- attendance_organizer.py
+++ attendance_organizer.py
@@ -21,6 +21,7 @@ class AttendanceOrganizer:
         self.status_var = tkinter.StringVar(self.root)
         self.status_label = tkinter.Label(
             self.root, textvariable=self.status_var)
+
         self.details_var = tkinter.StringVar(self.root)
         self.details_entry = tkinter.Entry(
             self.root, textvariable=self.details_var, state='readonly',
@@ -32,9 +33,11 @@ class AttendanceOrganizer:
         self.upload_path = tkinter.Entry(
             self.root, textvariable=self.upload_var, state='readonly',
             width=100)
+
         self.organize_data_button = tkinter.Button(
             self.root, text="Organize Data", state='disabled',
             command=self.organize_data)
+
         self.end_task_button = tkinter.Button(
             self.root, text="End Task", command=self.end_task)
 
@@ -46,16 +49,23 @@ class AttendanceOrganizer:
             self.root, textvariable=self.download_var, state='readonly',
             width=100)
 
+        self.reset_button = tkinter.Button(
+            self.root, text="Reset", command=self.reset)
+
     def display(self):
         self.upload_file_button.grid(row=0, column=0)
         self.upload_path.grid(row=0, column=1, sticky='w')
+
         self.end_task_button.grid(row=0, column=2)
 
         self.organize_data_button.grid(row=1, column=0)
+
         self.details_entry.grid(row=1, column=1, sticky='w')
 
         self.download_file_button.grid(row=2, column=0)
         self.download_path.grid(row=2, column=1, sticky='w')
+
+        self.reset_button.grid(row=1, column=2)
 
         self.root.mainloop()
 
@@ -64,7 +74,7 @@ class AttendanceOrganizer:
             value="Select a file to upload")
         filetypes = [('Comma Separated Values', '.csv')]
         filepath = tkinter.filedialog.askopenfilename(
-            parent=self.root, filetypes=filetypes)
+            parent=self.root, filetypes=filetypes, initialdir=os.getcwd())
         if not (filepath and os.path.splitext(filepath)[-1] == '.csv'):
             return
         self.upload_var.set(value=filepath)
@@ -79,6 +89,7 @@ class AttendanceOrganizer:
         self.organize_data_button.config(state='disabled')
         name_regex = re.compile(r'([A-Z][A-Za-z]+) ([A-Z][A-Za-z]+)')
         action_regex = re.compile(r'(Joined|Left)')
+        datetime_format = '%m/%d/%Y, %I:%M:%S %p'
         data = {}
         for row in self.values:
             full_name = name_regex.search(row[0])
@@ -86,16 +97,25 @@ class AttendanceOrganizer:
             try:
                 first, last = full_name.group(1), full_name.group(2)
                 action = action_name.group(1).title()
+                time = datetime.datetime.strptime(
+                    row[2], datetime_format).strftime('%m.%d%Y %H:%M:%S')
+            except (AttributeError, ValueError):
+                self.data = {}
+                self.details_var.set(
+                    value="An error occurred while processing the data. "
+                    "Check to make sure the file is correct")
+            try:
+                first, last = full_name.group(1), full_name.group(2)
+                action = action_name.group(1).title()
             except AttributeError:
                 self.data = {}
                 return
             try:
-                dt_obj = datetime.datetime.strptime(
-                    row[2], '%m/%d/%Y, %I:%M:%S %p')
+                dt_obj = datetime.datetime.strptime(row[2], datetime_format)
+                time = dt_obj.strftime('%m.%d.%Y %H:%M:%S')
             except ValueError:
                 self.data = {}
                 return
-            time = dt_obj.strftime('%m.%d.%Y %H:%M:%S')
             key = f"{last}, {first}"
             data.setdefault(key, {"Last": last, "First": first})
             data[key].setdefault(action, time)
@@ -109,7 +129,8 @@ class AttendanceOrganizer:
             value=f"Select a location to save file\t{self.details_var.get()}")
         filetypes = [('Comma Separated Values', '.csv')]
         filepath = tkinter.filedialog.asksaveasfilename(
-            parent=self.root, filetypes=filetypes)
+            parent=self.root, filetypes=filetypes, initialdir=os.getcwd())
+        self.download_var.set(value=filepath)
         with open(f"{filepath}.csv", 'w', newline='') as file:
             fieldnames = ["Last", "First", "Joined", "Left"]
             writer = csv.DictWriter(
@@ -122,6 +143,15 @@ class AttendanceOrganizer:
 
     def end_task(self):
         self.root.destroy()
+
+    def reset(self):
+        self.status_var.set('')
+        self.details_var.set('')
+        self.upload_file_button.config(state='normal')
+        self.upload_var.set('')
+        self.organize_data_button.config(state='disabled')
+        self.download_file_button.config(state='disabled')
+        self.download_var.set('')
 
 def main():
     organizer = AttendanceOrganizer()
