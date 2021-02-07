4234a4fa60de24ec02fbf607bc8900da0a2f8ec4
Jacob Lee <JLpython@outlook.com>
2021-02-07 11:28:30 -0800

    Refactor widget layout from attendance_organizer.py

--- widget.py
+++ widget.py
@@ -24,3 +24,88 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+import tkinter
+
+from attendance_organizer import Organizer
+
+
+class Widget(Organizer):
+
+    def __init__(self):
+        super().__init__()
+
+        self.root = tkinter.Tk()
+        self.root.attributes("-topmost", True)
+        self.root.title("Attendance Organizer")
+
+        self.status_var = tkinter.StringVar(self.root)
+        self.status_label = tkinter.Label(
+            self.root, textvariable=self.status_var
+        )
+
+        self.details_var = tkinter.StringVar(self.root)
+        self.details_entry = tkinter.Entry(
+            self.root, textvariable=self.details_var,
+            state="readonly", width=100
+        )
+
+        self.upload_button = tkinter.Button(
+            self.root, text="Upload File", command=self.upload_file
+        )
+        self.upload_var = tkinter.StringVar(self.root)
+        self.upload_entry = tkinter.Entry(
+            self.root, textvariable=self.upload_var,
+            state="readonly", width=100
+        )
+
+        self.organize_button = tkinter.Button(
+            self.root, text="Organize Data",
+            state="disabled", command=self.organize
+        )
+
+        self.end_task_button = tkinter.Button(
+            self.root, text="End Task", command=self.root.destroy
+        )
+
+        self.download_button = tkinter.Button(
+            self.root, text="Download File",
+            state="disabled", command=self.download_file
+        )
+        self.download_var = tkinter.StringVar(self.root)
+        self.download_entry = tkinter.Entry(
+            self.root, textvariable=self.download_var,
+            state="readonly", width=100
+        )
+
+        self.reset_button = tkinter.Button(
+            self.root, text="Reset", command=self.reset_widget
+        )
+
+    def display(self):
+        self.upload_button.grid(row=0, column=0, sticky="w")
+        self.upload_entry.grid(row=0, column=1, sticky="w")
+        self.end_task_button.grid(row=0, column=2, sticky="e")
+
+        self.organize_button.grid(row=1, column=0, sticky="w")
+        self.details_entry.grid(row=1, column=1, sticky="w")
+        self.reset_button.grid(row=1, column=2, sticky="e")
+
+        self.download_button.grid(row=2, column=0, sticky="w")
+        self.download_entry.grid(row=2, column=1, sticky="w")
+
+        self.root.mainloop()
+
+    def upload_file(self):
+        pass
+
+    def download_file(self):
+        pass
+
+    def reset_widget(self):
+        pass
+
+
+if __name__ == '__main__':
+    widget = Widget()
+    widget.display()
